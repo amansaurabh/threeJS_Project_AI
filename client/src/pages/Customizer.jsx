cb8434dc33079a51dded852d5951229b6ad6ac38
amansaurabh <amansaurabh127@gmail.com>
2023-04-04 18:56:27 +0530

    changed api key

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -54,7 +54,7 @@ const Customizer = () => {
     try {
       // call our backend to generate on ai image!
       setGeneratingImg(true);
-      const response = await fetch("http://localhost:8080/api/v1/dalle", {
+      const response = await fetch("https://project-threejs-ai-1u1p.onrender.com/api/v1/dalle", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
